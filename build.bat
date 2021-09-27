64e3622877c850035bf934bfa1d73f7b7477bdc5
Christoph Buelter <buelter.christoph@gmail.com>
2021-09-27 20:59:29 +0200

    Bump version

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @REM Update version as needed
-set version=0.5.0
+set version=0.5.1
 
 python -m eel --onefile --noconsole main.py frontend --icon build.ico --name hunt_showdown_outfitter_v%version%
