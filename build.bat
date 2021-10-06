2bb1e9527c57afdcb4b6d4b7ab251de079662c62
Christoph Buelter <buelter.christoph@gmail.com>
2021-10-06 17:28:17 +0200

    Bump version 0.7.0 -> 0.8.0

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @REM Update version as needed
-set version=0.7.0
+set version=0.8.0
 
 python -m eel --onefile --noconsole main.py frontend --icon build.ico --name hunt_showdown_outfitter_v%version%
